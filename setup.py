47bb0859fc647c576d414551fce5dbf7d36a32d2
Jan Baumgärtner <jan-baumgaertner@web.de>
2022-02-28 18:28:13 +0100

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+# setup.py
+import setuptools
+
+setuptools.setup(
+    name='behavior_circuits',
+    version='1.0.0',
+    author='Jan Baumgärtner',
+    license='MIT',
+    description='...',
+    install_requires=['numpy>=1.17.4'],
+    packages=['behavior_circuits']
+)
